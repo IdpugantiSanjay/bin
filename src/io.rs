fe508251b5c04b3484e7b6ea54d6847ddff98945
Sanjay Idpuganti <sanjay11@outlook.com>
2025-01-01 12:23:38 +0530

    feat: change paste order

--- src/io.rs
+++ src/io.rs
@@ -60,7 +60,7 @@ impl Store {
     }
 
     pub async fn get_all_pastes(&self) -> Result<Vec<Paste>, sqlx::Error> {
-        let pastes = sqlx::query_as::<_, Paste>("SELECT id, title, content FROM pastes")
+        let pastes = sqlx::query_as::<_, Paste>("SELECT id, title, content FROM pastes ORDER BY created_at DESC")
             .fetch_all(&self.0)
             .await?;
         Ok(pastes)
